a9902f5d72eee002416661b6167346ac39bc0bc6
Steffen Lindner <mail@steffen-lindner.de>
2015-09-18 13:31:08 +0200

    Add setuphandlers for slider

--- src/plonetheme/tango/setuphandlers.py
+++ src/plonetheme/tango/setuphandlers.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from plone import api
+import os
 
 
 def post_install(context):
@@ -6,10 +8,41 @@ def post_install(context):
     if context.readDataFile('plonethemetango_default.txt') is None:
         return
     # Do something during the installation of this package
+    portal = api.portal.get()
+    _create_content(portal)
+
+
+def _create_content(portal):
+    if not portal.get('slider-images', False):
+        slider = api.content.create(
+            type='Folder',
+            container=portal,
+            title=u'Slider',
+            id='slider-images'
+        )
+        for slider_number in range(1, 4):
+            slider_name = u'slider-{0}'.format(str(slider_number))
+            slider_image = api.content.create(
+                type='Image',
+                container=slider,
+                id=slider_name
+            )
+            slider_image.image = load_image(slider_number)
+        api.content.transition(obj=slider, transition='publish')
+
+
+def load_image(slider):
+    from plone.namedfile.file import NamedBlobImage
+    filename = os.path.join(os.path.dirname(__file__), 'theme', 'img',
+                            'slide-{0}.jpg'.format(slider))
+    return NamedBlobImage(
+        data=open(filename, 'r').read(),
+        filename=u'slide-{0}.jpg'.format(slider)
+    )
 
 
 def uninstall(context):
     """Uninstall script"""
     if context.readDataFile('plonethemetango_uninstall.txt') is None:
         return
-    # Do something during the uninstallation of this package
+        # Do something during the uninstallation of this package
